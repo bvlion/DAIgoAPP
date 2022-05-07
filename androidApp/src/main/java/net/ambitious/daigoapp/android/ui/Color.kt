a59127d3e3d30094f66c73113553eaf4ff6c2478
bvlion <bvltiggeari@ambitious-i.net>
2022-05-07 13:18:12 +0900

    setup colors

--- androidApp/src/main/java/net/ambitious/daigoapp/android/ui/Color.kt
+++ androidApp/src/main/java/net/ambitious/daigoapp/android/ui/Color.kt
@@ -2,7 +2,24 @@ package net.ambitious.daigoapp.android.ui
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val md_theme_light_primary = Color(0xFF000000)
+val md_theme_light_onPrimary = Color(0xFFeeeeee)
+val md_theme_light_secondary = Color(0xFFbd004e)
+val md_theme_light_onSecondary = Color(0xFFffffff)
+val md_theme_light_error = Color(0xFFba1b1b)
+val md_theme_light_onError = Color(0xFFffffff)
+val md_theme_light_background = Color(0xFFfcfcfc)
+val md_theme_light_onBackground = Color(0xFF201a1b)
+val md_theme_light_surface = Color(0xFFfcfcfc)
+val md_theme_light_onSurface = Color(0xFF201a1b)
+
+val md_theme_dark_primary = Color(0xFFffffff)
+val md_theme_dark_onPrimary = Color(0xFF424242)
+val md_theme_dark_secondary = Color(0xFFffb2c2)
+val md_theme_dark_onSecondary = Color(0xFF670027)
+val md_theme_dark_error = Color(0xFFffb4a9)
+val md_theme_dark_onError = Color(0xFF680003)
+val md_theme_dark_background = Color(0xFF201a1b)
+val md_theme_dark_onBackground = Color(0xFFecdfe0)
+val md_theme_dark_surface = Color(0xFF201a1b)
+val md_theme_dark_onSurface = Color(0xFFecdfe0)
