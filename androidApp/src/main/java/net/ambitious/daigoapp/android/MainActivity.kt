0564f358adbc2967d2f58ac937e7d82cfff68d33
bvlion <bvltiggeari@ambitious-i.net>
2022-05-14 07:58:42 +0900

    current display confirmed

--- androidApp/src/main/java/net/ambitious/daigoapp/android/MainActivity.kt
+++ androidApp/src/main/java/net/ambitious/daigoapp/android/MainActivity.kt
@@ -1,50 +1,92 @@
 package net.ambitious.daigoapp.android
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import net.ambitious.daigoapp.API
-import net.ambitious.daigoapp.android.ui.MyApplicationTheme
+import net.ambitious.daigoapp.android.compose.InputArea
+import net.ambitious.daigoapp.android.compose.LoadingCompose
+import net.ambitious.daigoapp.android.compose.SampleArea
+import net.ambitious.daigoapp.android.compose.sampleWords
+import net.ambitious.daigoapp.android.ui.AppTheme
+import net.ambitious.daigoapp.call.Result
 
 class MainActivity : ComponentActivity() {
+
+  private val words = mutableStateListOf<String>()
+  private val api = API()
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     setContent {
-      MyApplicationTheme {
+      AppTheme {
         Surface(
           modifier = Modifier.fillMaxSize(),
           color = MaterialTheme.colors.background
         ) {
-          val message = remember { mutableStateOf("test") }
-          Greeting(message.value)
-          API().getDaigo("努力大事") {
-            message.value = it.text
-          }
+
+          val input = remember { mutableStateOf("") }
+          val result = remember { mutableStateOf("") }
+          val loading = remember { mutableStateOf(false) }
+          val createButtonEnable = remember { mutableStateOf(false) }
+
+          AllViews(input, result, loading, createButtonEnable, words, api)
         }
+      }
+    }
+  }
+
+  override fun onResume() {
+    super.onResume()
+    api.getSamples {
+      if (it is Result.Success) {
+        words.addAll(it.data.samples)
+      }
+    }
+  }
+
+  override fun onDestroy() {
+    api.cancel()
+    super.onDestroy()
+  }
+}
+
+@Composable
+fun AllViews(
+  input: MutableState<String>,
+  result: MutableState<String>,
+  loading: MutableState<Boolean>,
+  createButtonEnable: MutableState<Boolean>,
+  words: List<String>,
+  api: API = API()
+) {
+  Box(Modifier.fillMaxSize()) {
+    LoadingCompose(loading.value)
+    Box(Modifier.align(Alignment.BottomCenter)) {
+      Column {
+        SampleArea(input, createButtonEnable, words)
+        InputArea(input, result, loading, createButtonEnable, api)
       }
     }
   }
 }
 
-@Composable
-fun Greeting(message: String) {
-  Text(message)
-}
-
-@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun DefaultPreview() {
-  MyApplicationTheme {
-    Greeting("test")
+  val input = remember { mutableStateOf("努力大事") }
+  val result = remember { mutableStateOf("DD") }
+  val loading = remember { mutableStateOf(true) }
+  val createButtonEnable = remember { mutableStateOf(true) }
+  AppTheme {
+    AllViews(input, result, loading, createButtonEnable, sampleWords)
   }
 }
