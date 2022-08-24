173fb0605222c28754675124cda4a71a37a54f0e
bvlion <bvltiggeari@ambitious-i.net>
2022-08-25 08:59:23 +0900

    add suppress `GradleDependency` for activity-compose

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -81,6 +81,8 @@ dependencies {
   implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
   implementation("androidx.compose.ui:ui-viewbinding:$composeVersion")
   implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
+
+  @Suppress("GradleDependency") // WIP
   implementation("androidx.activity:activity-compose:1.4.0")
 
   implementation("com.google.accompanist:accompanist-flowlayout:0.24.7-alpha")
