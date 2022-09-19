fcc8719d0cb3852daf10f126c9a4c99ff8ca5ae0
bvlion <bvltiggeari@ambitious-i.net>
2022-09-19 09:19:51 +0900

    delete warning

--- androidApp/src/main/java/net/ambitious/daigoapp/android/data/AppDatabase.kt
+++ androidApp/src/main/java/net/ambitious/daigoapp/android/data/AppDatabase.kt
@@ -3,7 +3,7 @@ package net.ambitious.daigoapp.android.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [History::class], version = 1)
+@Database(entities = [History::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun historyDao(): HistoryDao
 }
