74cd36f78ada8ffbcfe7662706d83d5c7171bfca
bvlion <bvltiggeari@ambitious-i.net>
2022-09-25 09:57:03 +0900

    update base libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ buildscript {
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     classpath("com.codingfeline.buildkonfig:buildkonfig-gradle-plugin:0.11.0")
 
-    classpath("com.google.gms:google-services:4.3.13")
-    classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.1")
+    classpath("com.google.gms:google-services:4.3.14")
+    classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
     classpath("com.google.firebase:firebase-appdistribution-gradle:3.0.3")
     classpath("com.github.triplet.gradle:play-publisher:3.7.0")
   }
