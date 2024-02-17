6842a150c82cd5caeece0b587306b6835c911aba
bvlion <bvltiggeari@ambitious-i.net>
2024-02-17 13:40:19 +0900

    add UI test

---
+++ shared/src/androidAndroidTest/kotlin/net/ambitious/daigoapp/APIClientTest.android.kt
@@ -0,0 +1,20 @@
+package net.ambitious.daigoapp
+
+actual class APIClientTest {
+    /** Check default host */
+    actual fun testHost() {
+    }
+
+    /** Test 4 connect */
+    actual fun testNoAuthClient() {
+    }
+
+    /** Test 4 connect and header */
+    actual fun testAuthClient() {
+    }
+
+    /** Test dispatcher */
+    actual fun testDispatcher() {
+    }
+
+}
